fa15c25ba7a10a108196b1405fdbb42b4f26a42c
ideawu <ideawu@local>
2014-08-07 15:22:39 +0800

    udpate

--- demo/index.php
+++ demo/index.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 define('APP_PATH', dirname(__FILE__) . '/app');
 // You can change IPHP_PATH
-define('IPHP_PATH', dirname(__FILE__) . '/../iphp');
+define('IPHP_PATH', dirname(__FILE__) . '/..');
 require_once(IPHP_PATH . '/loader.php');
 
 App::run();
