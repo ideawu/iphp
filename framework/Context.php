17a2890790a07a08847214370a3898bef01a246c
ideawu <ideawu@local>
2014-09-17 23:45:16 +0800

    update Context to support array op

--- framework/Context.php
+++ framework/Context.php
@@ -1,31 +1,11 @@
 <?php
 class Context
 {
-	private $__data = array();
-	
 	function as_array(){
-		return $this->__data;
-	}
-
-	function __set($name, $value){
-		$this->__data[$name] = $value;
-		$this->$name = $value;
-	}
-
-	function __get($name){
-		if (array_key_exists($name, $this->__data)) {
-			return $this->__data[$name];
+		$arr = array();
+		foreach($this as $k=>$v){
+			$arr[$k] = $v;
 		}
-		return null;
-	}
-
-	/**  PHP 5.1.0之后版本 */
-	function __isset($name){
-		return isset($this->__data[$name]);
-	}
-
-	/**  PHP 5.1.0之后版本 */
-	function __unset($name){
-		unset($this->__data[$name]);
+		return $arr;
 	}
 }
