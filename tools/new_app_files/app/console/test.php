d0d3a696ad25112daaf794f25b3c9f4baaaecb3b
ideawu <ideawu@local>
2014-11-07 16:39:11 +0800

    add console script template

---
+++ tools/new_app_files/app/console/test.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+define('APP_PATH', dirname(__FILE__) . '/..');
+define('IPHP_PATH', '/data/lib/iphp');
+require_once(IPHP_PATH . '/loader.php');
+App::init();
+
+// do something...
+
