b8286bbfdbaa04ddc1047f679992fa002ef4720b
ideawu <ideawu@me>
2016-03-31 12:42:24 +0800

    update template

--- tools/new_app_files/app/views/layout.tpl.php
+++ tools/new_app_files/app/views/layout.tpl.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="UTF-8" />
 	<title>iphp</title>
-	<meta name="description" content="iphp framework">
-	<meta name="keywords" content="iphp framework">
-	<link href="<?= _url('/css/style.css') ?>" rel="stylesheet">
+	<meta name="description" content="iphp framework" />
+	<meta name="keywords" content="iphp framework" />
+	<link href="<?= _url('/css/style.css') ?>" rel="stylesheet" />
 </head>
 <body>
 
@@ -13,7 +13,7 @@
 	<?php _view(); ?>
 		
 	<div class="footer">
-		Copyright &copy; 2014 <a href="http://github.com/ideawu/iphp">iphp</a>. All rights reserved.
+		Copyright&copy;2014-2016 <a href="http://github.com/ideawu/iphp">iphp</a>. All rights reserved.
 	</div>
 </div>
 <!-- /container -->
