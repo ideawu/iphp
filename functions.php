f15e64b49a65c8e2a00f5f846bcb1f212c6f50aa
ideawu <ideawu@me>
2016-04-06 19:43:15 +0800

    update

--- functions.php
+++ functions.php
@@ -127,8 +127,8 @@ function _redirect($url, $params_or_http_code=array()){
 		$url = _url($url);
 		$http_code = intval($params_or_http_code);
 	}
+	@header("Location: $url", true, $http_code);
 	throw new AppRedirectException($url, $http_code);
-	// @header("Location: $url", true, $http_code);
 	// App::_break();
 }
 
