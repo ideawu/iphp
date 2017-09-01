d3ec5f1681cfd73db9844f62fe442e404137cfbd
ideawu <ideawu@me>
2017-09-01 17:48:38 +0800

    update

--- tools/new_app_files/app/views/_error/ajax.tpl.php
+++ tools/new_app_files/app/views/_error/ajax.tpl.php
@@ -1,6 +1,6 @@
 <?php
 $msg = htmlspecialchars($_e->getMessage());
-if(strpos($msg, 'in SQL:') !== false || strpos($msg, 'db error') !== false){
+if(strpos($msg, 'in SQL:') !== false || strpos($msg, 'db error') !== false || get_class($_e) == 'SSDBException'){
 	Logger::error($_e);
 	$msg = 'db error';
 }
--- tools/new_app_files/app/views/_error/default.tpl.php
+++ tools/new_app_files/app/views/_error/default.tpl.php
@@ -10,7 +10,7 @@
 <h1 style="text-align: center;">
 	<?php
 		$msg = htmlspecialchars($_e->getMessage());
-		if(strpos($msg, 'in SQL:') !== false || strpos($msg, 'db error') !== false){
+		if(strpos($msg, 'in SQL:') !== false || strpos($msg, 'db error') !== false || get_class($_e) == 'SSDBException'){
 			Logger::error($_e);
 			$msg = 'db error';
 		}
